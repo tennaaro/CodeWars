7376b2268157ac7ff494898fdee5b5fa0447fe2e
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-12-08 15:28:20 -0500

    codewars multiples of 3 or 5

---
+++ multiples.js
@@ -0,0 +1,12 @@
+function solution(number){
+  let sum = 0;
+  if (number < 0) {
+    return 0
+  }
+  for (i=0; i < number; i++) {
+    if (i%3 === 0 || i%5 ===0){
+      sum += i
+    }
+  }
+  return sum
+}
