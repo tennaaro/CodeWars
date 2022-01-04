11260127ada1a4737a8b229f679f0db90bbaa54c
Aaron Tenn <aaron.will.tenn@gmail.com>
2022-01-03 23:46:17 -0500

    matrix nxn determinant

--- matrixDeterminant.js
+++ matrixDeterminant.js
@@ -7,7 +7,20 @@ function determinant(m) {
     return m[0][0] * m[1][1] - m[0][1] * m[1][0]
   }
   else {
+    let rval = 0;
+    for(let i = 0; i < m[0].length; i++) {
+      let copy = JSON.parse(JSON.stringify(m));
+      console.log("this is copy", copy)
+      let a = m[0][i]
 
+      let multiplier = (-1)**i
+  
+      copy.splice(0,1)
+      removeCol(copy, i)
+      console.log(copy)
+      rval += multiplier * a * determinant(copy)
+    }
+    return rval
   }
 };
 
@@ -21,12 +34,18 @@ const removeCol = function(arr, colIndex) {
   }
 }
 
-let m3 = [[1,2,3], [4,5,6], [7,8,9]]
-console.log(m3)
-// m3.splice(1,1)
+let m2 = [ [2,5,3], [1,-2,-1], [1, 3, 4]]
+console.log(determinant(m2))
+
+// let m3 = [[1,2,3], [4,5,6], [7,8,9]]
 // console.log(m3)
-removeCol(m3)
-console.log(m3)
+// m3.splice(0,1)
+// console.log(m3)
+// removeCol(m3, 2)
+// console.log(m3)
+
+// removeCol(m3)
+// console.log(m3)
 
 
 
